9285a0d244b358cee29254a75a80b2e84ad7b772
mateus040 <mateusgabrielmoreno264@gmail.com>
2025-02-07 20:29:49 -0300

    feat: adding animations

--- src/app/_components/about.tsx
+++ src/app/_components/about.tsx
@@ -6,10 +6,14 @@ import { WhatsappLogo } from "@phosphor-icons/react/dist/ssr";
 
 export function About() {
   return (
-    <section className="bg-[#FDF6ec] py-16">
+    <section className="bg-[#FDF6ec] py-16 overflow-hidden">
       <div className="container px-4 mx-auto">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
-          <div className="relative">
+          <div
+            className="relative"
+            data-aos="fade-up-right"
+            data-aos-delay="300"
+          >
             <div className="relative w-full h-[400px] rounded-3xl overflow-hidden">
               <Image
                 src={about1Img}
@@ -32,7 +36,11 @@ export function About() {
             </div>
           </div>
 
-          <div className="space-y-6 mt-10">
+          <div
+            className="space-y-6 mt-10"
+            data-aos="fade-up-left"
+            data-aos-delay="300"
+          >
             <h2 className="text-4xl font-bold">SOBRE</h2>
 
             <p>
--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -21,15 +21,20 @@ export function Hero() {
       <div className="container mx-auto pt-16 pb-16 md:pb-0 px-4 relative">
         <article className="grid grid-cols-1 lg:grid-cols-2 gap-8">
           <div className="space-y-6">
-            <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-10">
+            <h1
+              className="text-3xl md:text-4xl lg:text-5xl font-bold leading-10"
+              data-aos="fade-down"
+            >
               Seu pet merece cuidado, carinho e atenção especial.
             </h1>
-            <p className="lg:text-lg">
+            <p className="lg:text-lg" data-aos="fade-right">
               Oferecemos os melhores serviços para garantir o bem-estar e a
               felicidade do seu amigo de quatro patas.
             </p>
 
             <a
+              data-aos="fade-up"
+              data-aos-delay="500"
               href={`https://wa.me/556799998800?text=Olá vim pelo site e gostaria de mais informações`}
               className="bg-green-500 px-5 py-2 rounded-md font-semibold flex items-center justify-center w-fit gap-2"
               target="_blank"
